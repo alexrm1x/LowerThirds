e7fcc3efd215679067e0c6101a79de491264521a
Alejandro Guerrero <alexrm1x@gmail.com>
2022-02-11 11:39:12 +0100

    versión 1

--- js.js
+++ js.js
@@ -1,1 +1,9 @@
-document.getElementsByTagName("h1")[0].style.fontSize = "6vw";
+document.getElementsByTagName("h1")[0].style.fontSize = "2vw";
+document.getElementsByTagName("h2")[0].style.fontSize = "1vw";
+
+function myFunction() {    
+    let x = document.getElementById("formname").value;    
+    let y = document.getElementById("formposition").value;    
+    document.getElementById("idnombre").innerHTML = x;
+    document.getElementById("idcargo").innerHTML = y;
+  }
